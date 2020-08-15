907f7feedac6400ab2c63bfb0f5397e78de02599
luccasdev <lucasrti@hotmail.com>
2020-08-15 01:40:37 -0300

    Removing old code

--- src/main/java/luccas/dev/logmanager/repository/AccessLogRepository.java
+++ src/main/java/luccas/dev/logmanager/repository/AccessLogRepository.java
@@ -2,7 +2,6 @@ package luccas.dev.logmanager.repository;
 
 import luccas.dev.logmanager.model.AccessLog;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,8 +9,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional(readOnly = true)
 public interface AccessLogRepository extends CrudRepository<AccessLog, Long>, JpaSpecificationExecutor<AccessLog> {
-
-    @Query(value = "SELECT * FROM DOWMADOAWMDO", nativeQuery = true)
-    AccessLog findAccesslogWithQuery();
 
 }
